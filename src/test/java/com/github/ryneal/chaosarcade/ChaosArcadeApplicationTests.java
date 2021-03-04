1755c16769c2767716629f7acb5d44300c802f2c
ryan.neal <ryan.neal@bt.com>
2021-03-04 12:25:12 +0000

    fix broken test

--- src/test/java/com/github/ryneal/chaosarcade/ChaosArcadeApplicationTests.java
+++ src/test/java/com/github/ryneal/chaosarcade/ChaosArcadeApplicationTests.java
@@ -1,10 +1,15 @@
 package com.github.ryneal.chaosarcade;
 
+import io.kubernetes.client.openapi.ApiClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ChaosArcadeApplicationTests {
+
+	@MockBean
+	public ApiClient kubernetesClient;
 
 	@Test
 	void contextLoads() {
